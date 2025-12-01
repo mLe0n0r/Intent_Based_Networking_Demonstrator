49b6e6ecba30b72518f81a42b1f7ae083ab070ea
Maria Leonor Pinto Guedes <up202107691@edu.fe.up.pt>
2025-12-01 12:06:09 +0000

    New Methods

--- src/IntentController.py
+++ src/IntentController.py
@@ -36,6 +36,13 @@ class IntentController(app_manager.RyuApp):
             ("10.0.0.2", "10.0.0.1")
         ]
 
+    def allowed_communication(self, src_ip, dst_ip):
+        """
+        Returns true is the pair matches the defined allowed pair
+        """
+
+        return (src_ip, dst_ip) in self.allowed_pairs
+
     def learn_mac(self, dpid, src_mac, in_port):
         """
         Stores the MAC address of each host and the corresponding switch port.
@@ -43,6 +50,7 @@ class IntentController(app_manager.RyuApp):
         Arguments:
         dpid: switch identifier
         src_mac: Mac address of destination
+        in_port: switch port from where the packet arrived
         """
 
         self.mac_to_port.setdefault(dpid, {})
@@ -56,20 +64,33 @@ class IntentController(app_manager.RyuApp):
         dpid: switch identifier
         dst_mac: Mac address of destination
         dp: datapath (represents the switch)
-        in_port: switch port from where the packet arrived
         """
 
         if dst_mac in self.mac_to_port.get(dpid, {}):
             return self.mac_to_port[dpid][dst_mac] # if it had already learnt the destination's mac port
         return dp.ofproto.OFPP_FLOOD # else flood (sents to every port excluding the one from where it arrived)
 
+    def send_packet(self, dp, msg, in_port, actions):
+        """
+        Sends the packet back to the switch with instructions
+
+        Arguments:
+        dp: datapath
+        msg: original OpenFlow message sent by the switch
+        in_port: switch port from where the packet arrived
+        actions: list of action that should be applied to the packet
+        """
+    
+        parser = dp.ofproto_parser # to send OpenFlow rule
+        out = parser.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id, in_port=in_port, actions=actions, data=msg.data)
+        dp.send_msg(out)
 
     # @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER) # event handler
     # def packet_in_handler(self, ev):
     #     msg = ev.msg # OpenFlow that came from the switch
     #     dp = msg.datapath
-    #     parser = dp.ofproto_parser
-    #     ofp = dp.ofproto
+    #     parser = dp.ofproto_parser # to send OpenFlow rule
+    #     ofp = dp.ofproto # OpenFlow constants
 
     #     dpid = dp.id
     #     in_port = msg.match["in_port"]
